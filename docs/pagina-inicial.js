c335233519a2c3f6ed852811624b12d5d4da3790
Samuel Pereira <samucafernandes12@gmail.com>
2024-11-25 16:27:59 -0300

    Mudanças basicas

--- docs/pagina-inicial.js
+++ docs/pagina-inicial.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         const response = await fetch('http://localhost:5000/api/chamados/status');
         const data = await response.json();
 
-        // Inicializa um objeto com contagens zeradas
+        
         const statusCounts = {
             'Novo': 0,
             'Em atendimento': 0,
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", async () => {
             'Excluído': 0
         };
 
-        // Atualiza o objeto com os valores do banco de dados
+       
         data.forEach(status => {
             statusCounts[status._id] = status.count;
         });
 
-        // Atualiza os elementos HTML com as contagens
+        
         document.querySelectorAll('.status-item1').forEach(item => {
             const statusText = item.querySelector('span').innerText.trim();
             const countElement = item.querySelector('.status-count1');
